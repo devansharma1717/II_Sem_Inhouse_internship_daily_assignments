0f949542537aea2a9a48f6317e02f236bfbd8e58
devansharma1717 <devansharma1717@gmail.com>
2026-07-13 23:39:59 +0530

    Create 1.php

---
+++ 25ESKCS091/Day 10/1.php
@@ -0,0 +1,18 @@
+<?php session_start();
+include '../common/db_connect.php';
+$email=$_POST['email'];
+$password=$_POST['password'];
+$query="select* from admin where email='$email' and password='$password'";
+$result=mysqli_query($conn, $query);
+$row=mysqli_fetch_assoc($result);
+
+if(count($row)!==0){
+    $_SESION['admin']=$row;
+    $_SESSION['is_loggedin']=true;
+    header('location:students.php');
+    exit;
+
+}else{
+    header ('location:index.php');
+    exit;
+}
