60b1d6bf83b32c580cff94a1ef6c30aa6a604fa2
devansharma1717 <devansharma1717@gmail.com>
2026-07-14 01:20:56 +0530

    Create script_1.js

---
+++ 25ESKCS091/Day 7/script_1.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>this is mys java script</h1>
+
+    
+</body>
+<script>
+    console.log("this is me");
+    // docu
+    var number1=34;
+    var number2=32;
+    console.log(number1 + number2);
+    var arr=[1,2,3,4]
+</script>
+</html>
