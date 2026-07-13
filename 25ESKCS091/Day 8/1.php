abaea6c682cd9420abd1b5a8f467411f1000981a
devansharma1717 <devansharma1717@gmail.com>
2026-07-13 23:32:50 +0530

    Create 1.php

---
+++ 25ESKCS091/Day 8/1.php
@@ -0,0 +1,16 @@
+<?php
+$name="poorwansh";
+$college="skit,jaipur";
+$branch="cse";
+$age=20;
+$cgpa=9.71;
+echo ("hello , my name is " . $name);
+echo "<br>";
+echo ("i study at " . $college);
+echo "<br>";
+echo("my branch is " . $branch);
+echo "<br>";
+echo("my cgpa is " . $cgpa);
+echo "<br>";
+echo("my age is " . $age);
+?>
